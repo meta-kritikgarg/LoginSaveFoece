1ac27c9be4c4ff46ac01a87ca632570555c35c96
KRITIK GARG - Desktop Metacube <kritikgarg240194+git@gmail.com>
2017-06-23 15:16:50 +0530

    Add Node

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+
+
+gulp.task('useref', function(){
+  return gulp.src('*.html')
+    .pipe(useref())
+	.pipe(gulpIf('*.js', uglify()))
+	// Minifies only if it's a CSS file
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
